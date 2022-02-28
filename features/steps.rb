1422265445789e5f0877947a51be0e1effeed1b8
Yas <ykordjaz@gmail.com>
2022-02-28 18:12:52 +0100

    added healthcheck etc

--- features/steps.rb
+++ features/steps.rb
@@ -1,19 +1,24 @@
 require 'net/http'
-require 'uri'
+# require 'uri'
 # require 'json_matchers/rspec'
 
 Given('the service is running') do
-    uri = URI('http://127.0.0.1:3000/books')
-    Net::HTTP.get(uri) # => String
+    uri = URI('http://127.0.0.1:3000/healthcheck')
+    res = Net::HTTP.get_response(uri) 
+    # p res.code
+    expect(res.code).to eq "200"
+   
+
 end
-  
-  When('I send HTTP Get request') do
-    pending # Write code here that turns the phrase above into concrete actions
+
+When('I send HTTP Get request') do
+    uri = URI('http://127.0.0.1:3000/books')
+    # req = Net::HTTP.get(uri) # => String
+    @res = Net::HTTP.get_response(uri)
   end
   
   Then('I receive a valid {int} response code') do |int|
-  # Then('I receive a valid {float} response code') do |float|
-    pending # Write code here that turns the phrase above into concrete actions
+    expect(@res.code).to eq(int.to_s)
   end
   
   Then('I get confirmation that there are books in the database') do
