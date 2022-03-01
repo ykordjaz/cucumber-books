f141838854c21499939412030dc94d6af90c9ec7
Yas <ykordjaz@gmail.com>
2022-03-01 14:21:21 +0100

    testing the token

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,27 +1,29 @@
 require 'net/http'
-# require 'uri'
+require 'uri'
 # require 'json_matchers/rspec'
 
-Given('the service is running') do
+  Given('the service is running') do
     uri = URI('http://127.0.0.1:3000/healthcheck')
     res = Net::HTTP.get_response(uri) 
     # p res.code
     expect(res.code).to eq "200"
-   
+  end
 
-end
-
-When('I send HTTP Get request') do
+  When('I send HTTP Get request') do
     uri = URI('http://127.0.0.1:3000/books')
-    # req = Net::HTTP.get(uri) # => String
     @res = Net::HTTP.get_response(uri)
   end
   
   Then('I receive a valid {int} response code') do |int|
     expect(@res.code).to eq(int.to_s)
+    # expect(@res.code).to eq "200" also works
   end
   
   Then('I get confirmation that there are books in the database') do
-    pending # Write code here that turns the phrase above into concrete actions
+    expect(@res.body.size).to be > 0
+  end
+  
+  Then('I get confirmation that books match the book json schema') do
+    expect(@res).to match_response_schema("book".to_s)
   end
   
